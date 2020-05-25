40bbd24717ecdea8b5eb0d02b18d1f868c6aad54
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-05-25 17:08:07 +0800

    Add git override script

---
+++ setup-git.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git config --global user.name "lambdanadasomething"
+git config --global user.email "59504808+lambdanadasomething@users.noreply.github.com"
+git config --global credential.helper cache
