3e9e9cbedaf784c053be8bcaf690c51b6233fbdd
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-05-25 15:34:16 +0000

    Add sample project skeleton

---
+++ swagger-data/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to swagger-data
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ swagger-data/test/swagger_data/core_test.clj
@@ -0,0 +1,7 @@
+(ns swagger-data.core-test
+  (:require [clojure.test :refer :all]
+            [swagger-data.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
