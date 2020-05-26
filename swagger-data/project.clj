0de103caa88ab10591a6444a383480026c29b869
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-05-26 13:59:11 +0000

    Add dependencies for the experiment

--- swagger-data/project.clj
+++ swagger-data/project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [metosin/ring-swagger "0.26.2"]
+                 [frankiesardo/route-swagger "0.1.4"]
+                 [pedestal-api "0.3.4"]
+                 [swagger-spec "0.5.0"]
+                 [io.swagger.parser.v3/swagger-parser "2.0.20"]]
   :main ^:skip-aot swagger-data.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
