ac927056390216d0a46e158b1fb67c2053d1da28
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-06-22 15:21:22 +0000

    Work through the basics of reitit

--- reitit-test/project.clj
+++ reitit-test/project.clj
@@ -3,11 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[reifyhealth/lein-git-down "0.3.6"]]
+  :repositories [["public-github" {:url "git://github.com"}]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.pedestal/pedestal.service "0.5.8"]
                  [io.pedestal/pedestal.jetty "0.5.8"]
                  [metosin/reitit-pedestal "0.5.2"]
-                 [metosin/reitit "0.5.2"]]
+                 [metosin/reitit "0.5.2"]
+                 [metosin/malli "10ba8b963ac419ee33729315c3defb936587447c"]]
   :main ^:skip-aot reitit-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
---
+++ reitit-test/src/reitit_test/integration.clj
@@ -0,0 +1,60 @@
+(ns reitit-test.integration
+  (:require [reitit.core :as r]
+            [reitit.ring :as ring]
+            [reitit.spec :as rs]
+            [reitit.dev.pretty :as pretty]
+            [reitit.coercion :as coercion]
+            [reitit.coercion.malli]
+            [malli.core :as m]))
+
+(def router
+  (r/router
+   [["/api/ping" ::ping]
+    ["/api/orders/:id" ::order-by-id]]))
+
+(r/match-by-path router "/api/orders/45")
+
+(r/match-by-name router ::order-by-id {:id 3})
+
+(defn handler [_]
+  {:status 200, :body "ok"})
+
+(defn wrap [handler id]
+  (fn [request]
+    (update (handler request) :wrap (fnil conj '()) id)))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    ["/api" {:middleware [[wrap :api]]}
+     ["/ping" {:get handler
+               :name ::ping}]
+     ["/admin" {:middleware [[wrap :admin]]}
+      ["/users" {:get handler
+                 :post handler}]]])))
+
+(app {:request-method :get, :uri "/api/ping"})
+
+(r/router
+ ["/api" {:handler "identity"}]
+ {:validate rs/validate
+  :exception pretty/exception})
+
+; Coercion is more complicated...
+
+(m/validator [:string])
+
+;{:path {:company (m/validator [:string])
+;         :user-id (m/validator [int?])}}
+
+(def router2
+  (r/router
+   ["/:company/users/:user-id" {:name ::user-view
+                                :coercion reitit.coercion.malli/coercion
+                                :parameters {:path [:map 
+                                                    [:company :string]
+                                                    [:user-id [int?]]]}}]
+   {:compile coercion/compile-request-coercers}))
+
+(coercion/coerce!
+ (r/match-by-path router2 "/metosin/users/123"))
