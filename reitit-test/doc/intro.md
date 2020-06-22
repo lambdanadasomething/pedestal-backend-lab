09cb8d3ff0465a9d0c083d8221f8cd30fe4b9394
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-06-22 06:52:34 +0000

    Add reitit test project

---
+++ reitit-test/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to reitit-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ reitit-test/test/reitit_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns reitit-test.core-test
+  (:require [clojure.test :refer :all]
+            [reitit-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
