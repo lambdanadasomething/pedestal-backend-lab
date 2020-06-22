2beae452071697fd064355c01ab5d59c95d781c7
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-06-22 07:18:22 +0000

    Supple dependencies for reitit-test

--- reitit-test/project.clj
+++ reitit-test/project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.pedestal/pedestal.service "0.5.8"]
+                 [io.pedestal/pedestal.jetty "0.5.8"]
+                 [metosin/reitit-pedestal "0.5.2"]
+                 [metosin/reitit "0.5.2"]]
   :main ^:skip-aot reitit-test.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {}})
