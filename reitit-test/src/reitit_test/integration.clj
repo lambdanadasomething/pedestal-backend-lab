f6df2c154b3512cd8f9c49f5ca4fe85484aa71c3
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-06-23 06:42:50 +0000

    Basic pedestal integration

--- reitit-test/src/reitit_test/integration.clj
+++ reitit-test/src/reitit_test/integration.clj
@@ -1,2 +1,34 @@
-(ns reitit-test.integration)
+(ns reitit-test.integration
+  (:require [io.pedestal.http :as server]
+            [reitit.pedestal :as reitit-ped]
+            [reitit.http :as http]
+            [reitit.ring :as ring]))
 
+(defn interceptor [number]
+  {:enter (fn [ctx] (update-in ctx [:request :number] (fnil + 0) number))})
+
+(def routes
+  ["/api"
+   {:interceptors [(interceptor 1)]}
+
+   ["/number"
+    {:interceptors [(interceptor 10)]
+     :get {:interceptors [(interceptor 100)]
+           :handler (fn [req]
+                      {:status 200
+                       :body (select-keys req [:number])})}}]])
+
+(def server-config
+  (-> {::server/type :jetty
+     ::server/port 3000
+     ::server/join? false
+     ;; no pedestal routes
+     ::server/routes []}
+    (server/default-interceptors)
+    ;; swap the reitit router
+    (reitit-ped/replace-last-interceptor
+      (reitit-ped/routing-interceptor
+        (http/router routes)))
+    (server/dev-interceptors)))
+
+(def myserver (server/create-server server-config))
